cd067858ba4b9ff80793adf8f5b3c977a7f38347
Radosław Cymer <cymerrad@gmail.com>
2018-12-20 12:32:57 +0100

    Finally accepted permutation of magic keywords: module, declare, global

--- index.d.ts
+++ index.d.ts
@@ -1,73 +1,80 @@
-/**
+import { Provider } from "web3/providers";
+
+declare global {
+
+  /**
  * Globals
  */
-/// <reference types="chai" />
-/// <reference types="mocha" />
-declare const assert: Chai.AssertStatic;
-declare const expect: Chai.ExpectStatic;
+  const assert: Chai.AssertStatic;
+  const expect: Chai.ExpectStatic;
 
-declare function contract(name: string, test: (accounts: Truffle.Accounts) => void): void;
+  function contract(name: string, test: (accounts: Truffle.Accounts) => void): void;
 
-declare const artifacts: Truffle.Artifacts;
+  const artifacts: Truffle.Artifacts;
 
-declare const web3: any;
+  const web3: any;
 
-/**
- * Namespace
- */
-declare namespace Truffle {
-  type Accounts = string[];
+  /**
+   * Namespace
+   */
+  namespace Truffle {
+    type Accounts = string[];
 
-  interface TransactionDetails {
-    from: string;
-    gas?: number | string;
-    gasPrice?: number | string;
-    value?: number | string;
-  }
+    interface TransactionDetails {
+      from: string;
+      gas?: number | string;
+      gasPrice?: number | string;
+      value?: number | string;
+    }
 
-  export interface TransactionLog {
-    address: string;
-    args: any;
-    blockHash: string;
-    blockNumber: number;
-    event: string;
-    logIndex: number;
-    transactionHash: string;
-    transactionIndex: number;
-    type: string;
-  }
+    interface TransactionLog {
+      address: string;
+      args: any;
+      blockHash: string;
+      blockNumber: number;
+      event: string;
+      logIndex: number;
+      transactionHash: string;
+      transactionIndex: number;
+      type: string;
+    }
 
-  export interface TransactionResponse {
-    tx: string;
-    receipt: any;
-    logs: TransactionLog[];
-  }
+    interface TransactionResponse {
+      tx: string;
+      receipt: any;
+      logs: TransactionLog[];
+    }
 
-  interface Contract<T> extends ContractNew<any[]> {
-    deployed(): Promise<T>;
-    at(address: string): T;
-    address: string;
-    contractName: string;
-  }
+    interface Contract<T> extends ContractNew<any[]> {
+      'new'(...args: any[]): Promise<T>;
+      deployed(): Promise<T>;
+      at(address: string): T;
 
-  interface ContractInstance {
-    address: string;
-  }
+      // defaults(params: CallData): void;
+      setProvider(provider: Provider): void;
+      address: string;
+      contractName: string;
+    }
 
-  interface ContractNew<ARGs extends any[]> {
-    "new"(...args: ARGs): any;
-  }
+    interface ContractInstance {
+      address: string;
+    }
 
-  interface Deployer {
-    link(library: Truffle.Contract<any>, destination: Truffle.Contract<any>): Deployer;
-    link(library: Truffle.Contract<any>, destinations: Array<Truffle.Contract<any>>): Deployer;
-    deploy<T extends any[]>(c: ContractNew<T>, ...args: T): Deployer;
-  }
+    interface ContractNew<ARGs extends any[]> {
+      "new"(...args: ARGs): any;
+    }
 
-  type Migration = (deploy: Deployer, network: string, accounts: Accounts) => void;
+    interface Deployer {
+      link(library: Truffle.Contract<any>, destination: Truffle.Contract<any>): Deployer;
+      link(library: Truffle.Contract<any>, destinations: Array<Truffle.Contract<any>>): Deployer;
+      deploy<T extends any[]>(c: ContractNew<T>, ...args: T): Deployer;
+    }
 
-  // Wanna exact typings for your smartcontracts? Use typechain
-  interface Artifacts {
-    require<T = any>(name: string): T;
+    type Migration = (deploy: Deployer, network: string, accounts: Accounts) => void;
+
+    // Wanna exact typings for your smartcontracts? Use typechain
+    interface Artifacts {
+      require<T = any>(name: string): T;
+    }
   }
 }
